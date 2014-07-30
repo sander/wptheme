58723a8fa3b203d5ad9ae69ae8ecec747e9e1670
Sander Dijkhuis <mail@sanderdijkhuis.nl>
2014-07-30 10:56:30 +0200

    Initial commit
    Created with ‘lein new wptheme’.

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to wptheme
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/wptheme/core_test.clj
@@ -0,0 +1,7 @@
+(ns wptheme.core-test
+  (:require [clojure.test :refer :all]
+            [wptheme.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
